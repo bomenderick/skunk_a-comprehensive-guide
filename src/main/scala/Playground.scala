654e20945cc20931c3ca5077145110de09b871ff
Bomen Derick <bomenderick@gmail.com>
2024-02-19 20:00:09 +0100

    Update

---
+++ src/main/scala/Playground.scala
@@ -0,0 +1,9 @@
+import cats.effect.{ExitCode, IO, IOApp}
+
+/**
+  * Created by Bomen Derick.
+  */
+object Playground extends IOApp{
+
+  override def run(args: List[String]): IO[ExitCode] = ???
+}
